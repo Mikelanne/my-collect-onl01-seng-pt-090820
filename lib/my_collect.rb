4ff02e3e6768139953db11581159c9470101f9ee
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-09-17 21:31:06 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
+def my_collect(collection)
+  if block_given?
+    i = 0 
+    collection = []
+    while i < collection.length 
+    collection << yield(collection[i])
+      i += 1 
+    end
+    collection
+    end
+  end
 
